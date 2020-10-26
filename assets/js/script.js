b96a9d8e6bf134f3e3d2057a45abbdd148a343b4
Aleksandar Trifunovic <aleksandar.trifunovic96@outlook.com>
2020-10-26 05:47:52 +0100

    Update about me section, minor modification to general style

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,11 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
-!(function($) {
-    "use strict";
+!(function ($) {
+  "use strict";
 
-      // Nav Menu
-  $(document).on('click', '.nav-menu a, .mobile-nav a', function(e) {
+  // Nav Menu
+  $(document).on('click', '.nav-menu a, .mobile-nav a', function (e) {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var hash = this.hash;
       var target = $(hash);
@@ -28,7 +28,7 @@
 
         if (!$('#header').hasClass('header-top')) {
           $('#header').addClass('header-top');
-          setTimeout(function() {
+          setTimeout(function () {
             $("section").removeClass('section-show');
             $(hash).addClass('section-show');
 
@@ -54,46 +54,52 @@
     }
   });
 
-    // Activate/show sections on load with hash links
-    if (window.location.hash) {
-        var initial_nav = window.location.hash;
-        if ($(initial_nav).length) {
-          $('#header').addClass('header-top');
-          $('.nav-menu .active, .mobile-nav .active').removeClass('active');
-          $('.nav-menu, .mobile-nav').find('a[href="' + initial_nav + '"]').parent('li').addClass('active');
-          setTimeout(function() {
-            $("section").removeClass('section-show');
-            $(initial_nav).addClass('section-show');
-          }, 350);
+  // Activate/show sections on load with hash links
+  if (window.location.hash) {
+    var initial_nav = window.location.hash;
+    if ($(initial_nav).length) {
+      $('#header').addClass('header-top');
+      $('.nav-menu .active, .mobile-nav .active').removeClass('active');
+      $('.nav-menu, .mobile-nav').find('a[href="' + initial_nav + '"]').parent('li').addClass('active');
+      setTimeout(function () {
+        $("section").removeClass('section-show');
+        $(initial_nav).addClass('section-show');
+      }, 350);
+    }
+  }
+
+  // Mobile Navigation
+  if ($('.nav-menu').length) {
+    var $mobile_nav = $('.nav-menu').clone().prop({
+      class: 'mobile-nav d-lg-none'
+    });
+    $('body').append($mobile_nav);
+    $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="fas fa-bars"></i></button>');
+    $('body').append('<div class="mobile-nav-overly"></div>');
+
+    $(document).on('click', '.mobile-nav-toggle', function (e) {
+      $('body').toggleClass('mobile-nav-active');
+      $('.mobile-nav-toggle i').toggleClass('fas fa-bars fas fa-times');
+      $('.mobile-nav-overly').toggle();
+    });
+
+    $(document).click(function (e) {
+      var container = $(".mobile-nav, .mobile-nav-toggle");
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('.mobile-nav-toggle i').toggleClass('fas fa-bars fas fa-times');
+          $('.mobile-nav-overly').fadeOut();
         }
       }
+    });
+  } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
+    $(".mobile-nav, .mobile-nav-toggle").hide();
+  }
 
-    // Mobile Navigation
-    if ($('.nav-menu').length) {
-        var $mobile_nav = $('.nav-menu').clone().prop({
-          class: 'mobile-nav d-lg-none'
-        });
-        $('body').append($mobile_nav);
-        $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="fas fa-bars"></i></button>');
-        $('body').append('<div class="mobile-nav-overly"></div>');
-    
-        $(document).on('click', '.mobile-nav-toggle', function(e) {
-          $('body').toggleClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('fas fa-bars fas fa-times');
-          $('.mobile-nav-overly').toggle();
-        });
-    
-        $(document).click(function(e) {
-          var container = $(".mobile-nav, .mobile-nav-toggle");
-          if (!container.is(e.target) && container.has(e.target).length === 0) {
-            if ($('body').hasClass('mobile-nav-active')) {
-              $('body').removeClass('mobile-nav-active');
-              $('.mobile-nav-toggle i').toggleClass('fas fa-bars fas fa-times');
-              $('.mobile-nav-overly').fadeOut();
-            }
-          }
-        });
-      } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
-        $(".mobile-nav, .mobile-nav-toggle").hide();
-      }
+  // jQuery counterUp
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 1000
+  });
 })(jQuery);
